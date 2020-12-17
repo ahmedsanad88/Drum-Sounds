6761abda3483591d066e490bcb40d32185a56098
Ahmed ibrahim elsaeed hafez <ahmedibrahim151188@gmail.com>
2020-12-17 04:52:59 +0200

    first commit on DRUM Kit
    Enjoy.....

---
+++ index.js
@@ -0,0 +1,81 @@
+
+
+var selectedButton = document.querySelectorAll(".drum").length;
+
+for (var i=0 ; i < selectedButton; i++){
+
+	// var selectElement = document.querySelectorAll("button")[2].innerHTML;
+
+
+// Detecting button press.
+
+	document.querySelectorAll("button")[i].addEventListener("click", function (){
+
+		var container = this.innerHTML;
+
+		playSounds(container);
+
+		addAnimation(container);
+
+});
+
+}
+
+//  Detecting keyboard press.
+
+document.addEventListener("keydown", function (event) {
+
+	playSounds(event.key);
+
+	addAnimation(event.key);
+});
+
+function playSounds(key) {
+
+	switch(key){
+
+			case ("w"):
+				var tom1 = new Audio("sounds/tom-1.mp3");
+				tom1.play();
+			break;
+			case ("a"):
+				var tom2 = new Audio("sounds/tom-2.mp3");
+				tom2.play();
+			break;
+			case ("s"):
+				var tom3 = new Audio("sounds/tom-3.mp3");
+				tom3.play();
+			break;
+			case ("d"):
+				var tom4 = new Audio("sounds/tom-4.mp3");
+				tom4.play();
+			break;
+			case ("j"):
+				var crash = new Audio("sounds/crash.mp3");
+				crash.play();
+			break;
+			case ("k"):
+				var kick = new Audio("sounds/kick-bass.mp3");
+				kick.play();
+			break;
+			case ("l"):
+				var snare = new Audio("sounds/snare.mp3");
+				snare.play();
+			break;
+
+			defualt: console.log("press any rectangle!!");
+		}	
+}
+
+function addAnimation(animateKey) {
+
+	var activeKey = document.querySelector("." + animateKey);
+
+	activeKey.classList.add("pressed");
+
+	setTimeout(function (){
+
+		activeKey.classList.remove("pressed");
+	}, 100);
+
+}
